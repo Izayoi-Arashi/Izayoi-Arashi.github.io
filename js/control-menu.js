00e0b131a156dd7f6fda5b25f40f2646c407ce73
十六夜的Sakurua <107742034+Izayoi-Arashi@users.noreply.github.com>
2025-04-12 11:19:46 +0800

    Update control-menu.js

--- js/control-menu.js
+++ js/control-menu.js
@@ -8,7 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
       const musicPlayer = document.getElementById("music-player");
       const petalsToggle = document.getElementById("petals-toggle");
       const emailCopy = document.getElementById("email-copy");
- 
+
+      const announcementToggle = document.getElementById("announcement-toggle");
+      const announcementModal = document.getElementById("announcement-modal");
+      const closeModal = document.querySelector(".close-modal");
+      const announcementContent = document.getElementById("announcement-content");
+      const loadingIndicator = document.getElementById("loading-indicator");
       // 状态变量
       let isMenuOpen = localStorage.getItem("menu-open") === "true";
       let darkMode = localStorage.getItem("dark-mode");
